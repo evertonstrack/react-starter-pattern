c504e9201a79b60ba8fc5c494bb9f2f8e80d170b
evertonstrack <everton.strack@gmail.com>
2018-11-25 00:39:19 -0200

    Pages Structure proposal (Initial)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HomePage from './features/Home/Home.page';
+
+import './app.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header className="app-header">
+          <h1>React Stater Pattern</h1>
         </header>
+        <HomePage />
       </div>
     );
   }
